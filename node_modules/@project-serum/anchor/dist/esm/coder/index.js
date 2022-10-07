549b1e33b785943206fba68b81a100943e5b548a
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-10-07 16:07:39 +0800

    Deleted node files

--- node_modules/@project-serum/anchor/dist/esm/coder/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./borsh/index.js";
-export * from "./spl-token/index.js";
-export * from "./system/index.js";
-//# sourceMappingURL=index.js.map
--- node_modules/@project-serum/anchor/dist/esm/coder/spl-associated-token/events.js
+++
@@ -1,7 +0,0 @@
-export class SplAssociatedTokenEventsCoder {
-    constructor(_idl) { }
-    decode(_log) {
-        throw new Error("SPL associated token program does not have events");
-    }
-}
-//# sourceMappingURL=events.js.map
